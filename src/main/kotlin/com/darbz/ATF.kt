12e6738f4203b9d49df5f00959ef3ac87d40078d
Darbz <Darbzcharls@gmail.com>
2025-09-14 05:06:01 +0100

    Pretty Much Finished

--- src/main/kotlin/com/darbz/ATF.kt
+++ src/main/kotlin/com/darbz/ATF.kt
@@ -4,55 +4,47 @@ import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents
 import net.minecraft.client.MinecraftClient
-import net.minecraft.client.network.ClientPlayNetworkHandler
-
 
 object ATF : ModInitializer {
-	val client = MinecraftClient.getInstance().networkHandler
-
-
 	var Enabled = false
+
 	override fun onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-
-		ATF.info("-------------------------------\n=                             =" +
+		info("-------------------------------\n=                             =" +
 				"\n=     ANTI FREECAM LOADED     =\n=                             =\n-------------------------------")
 
-		ClientPlayConnectionEvents.DISCONNECT.register(ClientPlayConnectionEvents.Disconnect { handler: ClientPlayNetworkHandler?, client: MinecraftClient? ->
+		// Reset on disconnect
+		ClientPlayConnectionEvents.DISCONNECT.register { _, _ ->
 			println("Disconnected from server!")
 			Enabled = false
-		})
+		}
 
-		ClientTickEvents.END_CLIENT_TICK.register(ClientTickEvents.EndTick { client: MinecraftClient? ->
-			val player = client?.player ?: return@EndTick
+		// Respawn automatically on death
+		ClientTickEvents.END_CLIENT_TICK.register { client ->
+			val player = client?.player ?: return@register
 			if (player.isDead) {
 				player.requestRespawn()
 			}
-		})
 
-
-
-
-
-
+		}
 	}
 
 	fun info(string: String) {
 		println(string)
 	}
 
-
-	fun sendChatCommand(s: String) {
-        client!!.sendChatCommand(s)
-
+	fun sendChatCommand(command: String) {
+		val client = MinecraftClient.getInstance()
+		val networkHandler = client.networkHandler ?: return
+		client.execute {
+			networkHandler.sendChatCommand(command)
+		}
 	}
 
-	fun sendChatMessage(s: String) {
-		client!!.sendChatMessage(s)
+	fun sendChatMessage(message: String) {
+		val client = MinecraftClient.getInstance()
+		val player = client.player ?: return
+		client.execute {
+			client.networkHandler?.sendChatMessage(message)
+		}
 	}
-
-
 }
--- src/main/kotlin/com/darbz/data/HandleData.kt
+++ src/main/kotlin/com/darbz/data/HandleData.kt
@@ -2,38 +2,40 @@ package com.darbz.data
 
 import com.darbz.ATF
 import net.minecraft.client.MinecraftClient
+import net.minecraft.client.gui.DrawContext
+import net.minecraft.client.gui.screen.Screen
+import net.minecraft.text.Text
 import net.minecraft.world.GameMode
+
 
 class HandleData {
 
     companion object {
         val client = MinecraftClient.getInstance()
+
         @JvmStatic
         fun parseData(s: String) {
-            val data = s.split("#")
+            val data = s.split("_")
             val mode = Integer.parseInt(data[1])
 
             when (mode) {
                 0 -> {
                     ATF.Enabled = true
                     ATF.sendChatCommand("verify_mod")
+                    return
                 }
 
                 1 -> {
-                    if (ATF.Enabled && client.interactionManager!!.currentGameMode != GameMode.SPECTATOR) {
-
-                        client.setCameraEntity(client.player)
-
-
+                    if (client.getCameraEntity() != client.player && client.interactionManager != null &&
+                        client.interactionManager!!.getCurrentGameMode() != GameMode.SPECTATOR
+                    ) {
+                        client.cameraEntity = client.player
+                        return
                     }
-
-
 
                 }
             }
-
-    }
-
-
+        }
     }
 }
+
